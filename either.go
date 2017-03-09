136ed55531c678fed6735dc96486b17b97f74cee
Dmitry Moskowski <me+parse@corpix.ru>
2017-03-09 09:54:17 +0000

    Moved license after package keyword

--- either.go
+++ either.go
@@ -1,3 +1,5 @@
+package parse
+
 // Copyright © 2017 Dmitry Moskowski
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -17,7 +19,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-package parse
 
 type Either struct {
 	id    string
