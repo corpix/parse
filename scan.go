97f03efe400296326edc4e28b5e309546aa8ff22
Dmitry Moskowski <me+parse@corpix.ru>
2017-03-10 01:39:20 +0000

    Added *Tree scanner

---
+++ scan.go
@@ -0,0 +1,54 @@
+package parse
+
+// Copyright © 2017 Dmitry Moskowski
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+func Scan(tree *Tree, fn func(*Tree)) {
+	var (
+		current *Tree
+		stack   = []*Tree{}
+	)
+
+	if tree == nil {
+		return
+	}
+
+	current = tree
+	for {
+
+		fn(current)
+
+		if current.Childs != nil {
+			for _, v := range current.Childs {
+				stack = append(
+					stack,
+					v,
+				)
+			}
+		}
+
+		if len(stack) == 0 {
+			break
+		}
+
+		current = stack[0]
+		stack = stack[1:]
+	}
+}
