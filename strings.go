174575ac5ba5b91b63e0b5eed882ad356e2ee74e
Dmitry Moskowski <me@corpix.ru>
2017-07-04 02:29:40 +0000

    Added funcs to check chain prefix and suffix

--- strings.go
+++ strings.go
@@ -35,6 +35,38 @@ func indent(s string, character string, size int) string {
 	return strings.Join(lines, newLine)
 }
 
+// EqualSlicesFoldPrefix checks that a is prefixed or equal with prefix
+// via EqualSlicesFold.
+func EqualSlicesFoldPrefix(a, prefix []string) bool {
+	l, lprefix := len(a), len(prefix)
+	switch {
+	case lprefix == 0:
+		return true
+	case l < lprefix:
+		return false
+	case l > lprefix:
+		return EqualSlicesFold(a[:lprefix], prefix)
+	default:
+		return EqualSlicesFold(a, prefix)
+	}
+}
+
+// EqualSlicesFoldSuffix checks that a is suffixed or equal with suffix
+// via EqualSlicesFold.
+func EqualSlicesFoldSuffix(a, suffix []string) bool {
+	l, lsuffix := len(a), len(suffix)
+	switch {
+	case lsuffix == 0:
+		return true
+	case l < lsuffix:
+		return false
+	case l > lsuffix:
+		return EqualSlicesFold(a[l-lsuffix:], suffix)
+	default:
+		return EqualSlicesFold(a, suffix)
+	}
+}
+
 // EqualSlicesFold checks that all elements from a
 // exists in b with strings.EqualFold.
 func EqualSlicesFold(a, b []string) bool {
--- strings_test.go
+++ strings_test.go
@@ -27,6 +27,136 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestEqualSlicesFoldPrefix(t *testing.T) {
+	samples := []struct {
+		a     []string
+		b     []string
+		equal bool
+	}{
+		{
+			[]string{},
+			[]string{},
+			true,
+		},
+		{
+			[]string{"foo", "bar"},
+			[]string{},
+			true,
+		},
+		{
+			[]string{"foo", "bar"},
+			[]string{"foo", "bar"},
+			true,
+		},
+		{
+			[]string{"A", "a"},
+			[]string{"a", "a"},
+			true,
+		},
+		{
+			[]string{"a"},
+			[]string{"b"},
+			false,
+		},
+		{
+			[]string{"a", "a"},
+			[]string{"a"},
+			true,
+		},
+		{
+			[]string{"a", "b", "c"},
+			[]string{"a", "b"},
+			true,
+		},
+		{
+			[]string{"a"},
+			[]string{"a", "b"},
+			false,
+		},
+		{
+			[]string{"a", "c"},
+			[]string{"a", "b"},
+			false,
+		},
+	}
+
+	for k, sample := range samples {
+		msg := spew.Sdump(k, sample)
+
+		assert.EqualValues(
+			t,
+			sample.equal,
+			EqualSlicesFoldPrefix(sample.a, sample.b),
+			msg,
+		)
+	}
+}
+
+func TestEqualSlicesFoldSuffix(t *testing.T) {
+	samples := []struct {
+		a     []string
+		b     []string
+		equal bool
+	}{
+		{
+			[]string{},
+			[]string{},
+			true,
+		},
+		{
+			[]string{"foo", "bar"},
+			[]string{},
+			true,
+		},
+		{
+			[]string{"foo", "bar"},
+			[]string{"foo", "bar"},
+			true,
+		},
+		{
+			[]string{"A", "a"},
+			[]string{"a", "a"},
+			true,
+		},
+		{
+			[]string{"a"},
+			[]string{"b"},
+			false,
+		},
+		{
+			[]string{"a", "a"},
+			[]string{"a"},
+			true,
+		},
+		{
+			[]string{"a", "b", "c"},
+			[]string{"b", "c"},
+			true,
+		},
+		{
+			[]string{"a"},
+			[]string{"b", "a"},
+			false,
+		},
+		{
+			[]string{"a", "c"},
+			[]string{"a", "b"},
+			false,
+		},
+	}
+
+	for k, sample := range samples {
+		msg := spew.Sdump(k, sample)
+
+		assert.EqualValues(
+			t,
+			sample.equal,
+			EqualSlicesFoldSuffix(sample.a, sample.b),
+			msg,
+		)
+	}
+}
+
 func TestEqualSlicesFold(t *testing.T) {
 	samples := []struct {
 		a     []string
